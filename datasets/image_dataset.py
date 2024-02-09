5f707a153772354290f22fd4ec0d4fd2f2262d14
MANISH <gyawalimanish12345@gmail.com>
2024-02-09 12:12:52 +0545

    changes

--- datasets/image_dataset.py
+++ datasets/image_dataset.py
@@ -16,36 +16,6 @@ from os.path import join
 from PIL import Image
 
 
-class OtitisMedia(Dataset):
-    def __init__(self, csv_file):
-        train_files, train_labels = read_as_csv(csv_file)
-        
-        self.imgs =  np.array([image_transforms(file, label) for file, label in zip(train_files, train_labels)])
-        self.labels= np.array([label_to_index(label) for label in train_labels]) 
-    
-    def __len__(self ):
-        return(len(self.labels))   
-
-
-    
-    def __getitem__(self, label=0):
-        return(self.imgs[label], self.labels[label])
-
-        self.imgs = np.array(
-            [
-                image_transforms(file, label)
-                for file, label in zip(train_files, train_labels)
-            ]
-        )
-        self.labels = np.array([label_to_index(label) for label in train_labels])
-
-    def __len__(self):
-        return len(self.labels)
-
-    def __getitem__(self, label=1):
-        return (self.imgs[label], self.labels[label])
-
-
 class ImageDataset(Dataset):
     def __init__(self, csv_path, transforms=None):
         images, labels = read_as_csv(csv_path)
